244b3575a336b5e320834a333e699176b947beb0
Pavel Pergamenshchik <ppergame@gmail.com>
2026-06-27 09:00:26 -0700

    ffmpeg binaries

--- flake.nix
+++ flake.nix
@@ -19,8 +19,18 @@
     pkgs = import nixpkgs {
       inherit system;
       overlays = [rust-overlay.overlays.default];
+      config.allowUnfreePredicate = pkg: let
+        name = nixpkgs.lib.getName pkg;
+      in
+        name == "android-sdk-ndk"
+        || name == "ndk"
+        || nixpkgs.lib.hasPrefix "aarch64-unknown-linux-android-" name
+        || nixpkgs.lib.hasPrefix "android-sdk-" name
+        || nixpkgs.lib.hasPrefix "android-ndk" name
+        || nixpkgs.lib.hasPrefix "platform-tools" name;
     };
     v8 = import ./nix/v8.nix {inherit pkgs;};
+    ffmpegVp9 = import ./nix/ffmpeg-vp9.nix {inherit pkgs;};
     codex = import ./nix/codex.nix {inherit pkgs;};
     rustToolchain =
       pkgs.rust-bin.stable.latest.default.override
@@ -55,6 +65,8 @@
       D8_LINUX64 = "${v8.linux64}/d8";
       D8_ANDROID_ARM32 = "${v8.androidArm32}/d8";
       D8_ANDROID_ARM64 = "${v8.androidArm64}/d8";
+      FFMPEG_VP9_LINUX64 = "${ffmpegVp9.linux64}/bin/ffmpeg";
+      FFMPEG_VP9_ANDROID_ARM64 = "${ffmpegVp9.androidArm64}/bin/ffmpeg";
       SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
       NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
       NODE_EXTRA_CA_CERTS = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
---
+++ nix/ffmpeg-vp9.nix
@@ -0,0 +1,144 @@
+{pkgs}: let
+  inherit (pkgs) lib;
+
+  android = pkgs.pkgsCross.aarch64-android-prebuilt;
+
+  commonConfigureFlags = [
+    "--prefix=${placeholder "out"}"
+    "--bindir=${placeholder "out"}/bin"
+
+    "--disable-everything"
+    "--disable-autodetect"
+    "--disable-doc"
+    "--disable-debug"
+    "--disable-network"
+    "--disable-shared"
+    "--enable-static"
+    "--enable-pic"
+    "--enable-pthreads"
+    "--enable-runtime-cpudetect"
+    "--enable-optimizations"
+
+    "--enable-ffmpeg"
+    "--disable-ffplay"
+    "--disable-ffprobe"
+    "--disable-avdevice"
+    "--disable-swresample"
+    "--disable-swscale"
+
+    "--enable-decoder=vp9"
+    "--enable-demuxer=ivf"
+    "--enable-demuxer=matroska"
+    "--enable-protocol=file"
+    "--enable-protocol=pipe"
+
+    "--enable-muxer=rawvideo"
+    "--enable-muxer=yuv4mpegpipe"
+    "--enable-encoder=rawvideo"
+    "--enable-encoder=wrapped_avframe"
+
+    "--disable-hwaccels"
+    "--disable-mediacodec"
+    "--disable-jni"
+  ];
+
+  mkFfmpegVp9 = {
+    pname,
+    stdenv,
+    nativeBuildInputs,
+    targetConfigureFlags,
+    preConfigure ? "",
+    dontPatchELF ? false,
+    noAuditTmpdir ? false,
+    platforms,
+  }:
+    stdenv.mkDerivation {
+      inherit pname preConfigure dontPatchELF noAuditTmpdir;
+      version = pkgs.ffmpeg.version;
+
+      src = pkgs.ffmpeg.src;
+
+      strictDeps = true;
+      inherit nativeBuildInputs;
+
+      dontDisableStatic = true;
+      configurePlatforms = [];
+      configureFlags = commonConfigureFlags ++ targetConfigureFlags;
+
+      buildFlags = ["ffmpeg"];
+
+      installPhase = ''
+        runHook preInstall
+
+        install -Dm755 ffmpeg "$out/bin/ffmpeg"
+        install -d "$out/share/${pname}"
+        if [ -f ffbuild/config.log ]; then
+          install -Dm644 ffbuild/config.log "$out/share/${pname}/config.log"
+        fi
+        cat > "$out/share/${pname}/usage.txt" <<'EOF'
+Example decode commands:
+
+  ffmpeg -threads 1 -i input.webm -f yuv4mpegpipe -pix_fmt yuv420p output.y4m
+  ffmpeg -threads 1 -i input.ivf -f rawvideo -pix_fmt yuv420p output.yuv
+
+Use yuv4mpegpipe when the collector wants dimensions/frame boundaries in-band.
+Use rawvideo when width/height/pixel format are already known out-of-band.
+EOF
+
+        runHook postInstall
+      '';
+
+      dontStrip = false;
+      enableParallelBuilding = true;
+
+      meta = {
+        description = "Minimal FFmpeg CLI with native VP9 decode, IVF/WebM input, and raw YUV output";
+        homepage = "https://ffmpeg.org/";
+        license = lib.licenses.lgpl21Plus;
+        mainProgram = "ffmpeg";
+        inherit platforms;
+      };
+    };
+in {
+  linux64 = mkFfmpegVp9 {
+    pname = "ffmpeg-linux64-vp9";
+    stdenv = pkgs.stdenv;
+    nativeBuildInputs = [
+      pkgs.nasm
+      pkgs.pkg-config
+    ];
+    targetConfigureFlags = [
+      "--target-os=linux"
+      "--arch=x86_64"
+      "--cpu=generic"
+      "--pkg-config=${lib.getExe pkgs.pkg-config}"
+      "--x86asmexe=${lib.getExe pkgs.nasm}"
+    ];
+    platforms = ["x86_64-linux"];
+  };
+
+  androidArm64 = mkFfmpegVp9 {
+    pname = "ffmpeg-android-arm64-vp9";
+    stdenv = android.stdenv;
+    nativeBuildInputs = [
+      android.buildPackages.pkg-config
+    ];
+    preConfigure = ''
+      configureFlagsArray+=("--extra-ldexeflags=-static -no-pie -Wl,-z,max-page-size=16384 -Wl,-z,common-page-size=16384")
+    '';
+    targetConfigureFlags = [
+      "--target-os=android"
+      "--arch=aarch64"
+      "--cpu=generic"
+      "--cross-prefix=${android.stdenv.cc.targetPrefix}"
+      "--cc=${android.stdenv.cc.targetPrefix}clang"
+      "--cxx=${android.stdenv.cc.targetPrefix}clang++"
+      "--host-cc=${pkgs.stdenv.cc}/bin/cc"
+      "--pkg-config=${android.buildPackages.pkg-config}/bin/${android.buildPackages.pkg-config.targetPrefix}pkg-config"
+      "--enable-cross-compile"
+    ];
+    dontPatchELF = true;
+    noAuditTmpdir = true;
+    platforms = ["aarch64-linux"];
+  };
+}
